61fe7d1a95852f5b493610226673a9ccb170568f
Austin <austinowen173@gmail.com>
2021-01-13 13:27:09 -0500

    Initial commit

---
+++ src/Loop.java
@@ -0,0 +1,18 @@
+public class Loop {
+    public static void main(String[] args) {
+        for (int i =100; i>=0; i-=5)
+            System.out.print(i);
+
+
+        for (int i = 0; i<=100; i+=10)
+        {
+            if (i%20==0)
+                System.out.println("java");
+            else
+                System.out.print(i);
+        }
+
+        }
+        }
+
+
